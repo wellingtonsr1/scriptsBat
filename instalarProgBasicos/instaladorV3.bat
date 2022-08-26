d6c4e2a422f706f77159781388f6d56abd36bd34
wellingtonsr1 <97860975+wellingtonsr1@users.noreply.github.com>
2022-08-26 09:02:25 -0300

    Update instaladorV3.bat

--- instalarProgBasicos/instaladorV3.bat
+++ instalarProgBasicos/instaladorV3.bat
@@ -16,41 +16,16 @@ rem | -	https://www.youtube.com/watch?v=FtNzdOKk0kM							                      
 rem +------------------------------------------------------------------------------------------------------------------+
 
 
-rem ---------------------------------- Menu -------------------------------------
-:menu
-cls
-echo =======================================
-echo            MENU TAREFAS
-echo =======================================
-echo  1. Instalar os programas
-rem echo  2. Renomear o PC e coloca-lo no Dominio
-echo  2. Configurar a rede
-echo  3. Executar 1 e 2              
-echo  4. Sair                        
-echo =======================================
-
-set opMenu=
-set /p opMenu=Escolha uma opcao: 
-
-if not defined opMenu goto opcao5
-
-if %opMenu% equ 1 goto opcao1
-if %opMenu% equ 2 goto opcao2
-if %opMenu% equ 3 goto opcao3
-if %opMenu% equ 4 goto opcao4
-if %opMenu% GEQ 5 goto opcao5
-rem ----------------------------- Fim Menu ---------------------------------------
-
 
 rem ----------------------- Função instalar programas ----------------------------
-:opcao1
+:menu
 cls
 echo ==================================
 echo *      Instalar programas        *
 echo ==================================
 echo  1. Usar lista padrao 
 echo  2. Usar lista personalizada  
-echo  3. Voltar a tela principal                              
+echo  3. Sair                             
 echo ==================================
 
 set opInstall=
@@ -61,7 +36,7 @@ if not defined opInstall goto opcao6
 if %opInstall% GTR 3 goto opcao6
 if %opInstall% equ 1 set listaProgramas=listaPadrao.txt
 if %opInstall% equ 2 set listaProgramas=listaPersonalizada.txt 
-if %opInstall% equ 3 goto menu 
+if %opInstall% equ 3 goto opcao4 
 
 
 if not exist "C:\ProgramData\chocolatey" (
@@ -86,7 +61,13 @@ for /F "tokens=*" %%A in (instalarProgBasicos\%listaProgramas%) do (
 
 rem Instalar em modo silencioso (Ex.: CaminhoCompleto\NomePrograma.extensão /SILENT)
 instalarProgBasicos\ProgramasExtras\"setup_64_rev_101.exe" /SILENT 
-instalarProgBasicos\ProgramasExtras\"LibreOffice_7.2.6_Win_x64.msi" /passive
+
+rem ================================== ATENÇÃO!!! ==============================================
+rem 	Se houver algum problema para instalar/baixar o libreoffice usando o "chocolatey", 
+rem     descomente a linha abaixo e exclua o "libreoffice-still" da listaPadrao.txt
+rem ============================================================================================
+rem instalarProgBasicos\ProgramasExtras\"LibreOffice_7.2.7_Win_x64.msi" /passive
+
 instalarProgBasicos\ProgramasExtras\"Instala Simplestec.exe" /SILENT
 
 echo Programas instalados.
@@ -96,78 +77,13 @@ echo Adicionando rota...
 route -p ADD 10.96.0.0 MASK 255.255.255.0 10.39.0.2 metric 2
 route -p ADD 10.96.0.206 MASK 255.255.255.255 10.39.0.106 metric 1
 
-if %opMenu% equ 3 (
+if %opInstall% equ 3 (
 	goto restart
 ) else (
 	pause
 	goto menu
 )
 rem ------------------------------ Fim instalar -------------------------------------
-
-
-rem --------------------------- Função Configurar -----------------------------------
-:opcao2
-cls
-echo ===========================================
-echo *          Configurar a rede              *
-echo ===========================================
-
-rem set /p novoNomePC="Informe o novo nome do Computador: " 
-rem set /p interface="Interface: " || set "interface=Ethernet"
-set /p ip="Endereco IP: "
-set /p mask="Mascara de rede: "
-set /p gateway="Gateway: "
-set /p dns="DNS: "
-rem set /p dominio="Dominio: "
-
-rem Configura IP, Mascara e o Gateway
-netsh interface ip set address name="Ethernet" static %ip% %mask% %gateway%
-
-rem 'netsh' executou com erro?
-if %errorlevel% neq 0 (
-	echo Erro ao configurar parametros da rede
-	pause
-	goto menu
-)
-
-rem Configura o DNS primario
-netsh interface ip set dnsservers name="Ethernet" static %dns%
-
-rem 'netsh' executou com erro?
-if %errorlevel% neq 0 (
-	echo Erro ao configurar o DNS
-	pause
-	goto menu
-)
-
-rem Renomeia o computador e o coloca do dominio
-rem powershell.exe Add-Computer -DomainName %dominio% -NewName %novoNomePC%
-
-rem 'powershell.exe' executou com erro?
-rem if %errorlevel% neq 0 (
-rem	echo Erro ao renomear/colocar no dominio
-rem 	pause
-rem goto menu
-rem )
-
-echo Adicionando rota...           
-route delete 10.96.0.0
-route -p ADD 10.96.0.0 MASK 255.255.255.0 10.39.0.2 metric 2
-route -p ADD 10.96.0.206 MASK 255.255.255.255 10.39.0.106 metric 1
-echo Rota adicionada.  
-
-if %opMenu% equ 3 (goto opcao1) else (goto restart)
-rem ------------------------------ Fim Configurar ----------------------------------------
-
-
-rem ------------------------------- Opções 1 e 2 ---------------------------------------
-:opcao3
-cls
-echo ==================================
-echo *  Executando os passos 1 e 2... *
-echo ==================================
-goto :opcao2
-rem --------------------------------- Fim opções ----------------------------------------
 
 
 rem ------------------------------ Função reiniciar --------------------------------------
@@ -185,7 +101,7 @@ rem ---------------------------------- Fim sair --------------------------------
 
 
 rem -------------- Função valor inválido para o menu principal ----------------------------
-:opcao5
+:opcao6
 cls
 echo ===============================================
 echo * Opcao Invalida! Escolha outra opcao do menu *
@@ -194,15 +110,3 @@ echo.
 pause
 goto menu
 rem ------------------- Fim valor inválido menu principal ---------------------------------
-
-
-rem ----------------- Função valor inválido menu instalação--------------------------------
-:opcao6
-cls
-echo ===============================================
-echo *   Opcao Invalida! Escolha uma das listas    *
-echo ===============================================
-echo.
-pause
-goto opcao1
-rem ------------------ Fim valor inválido menu instalação ---------------------------------
